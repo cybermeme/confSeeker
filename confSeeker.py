8c14fcd84ecfed2059b44581cf020a087e359db3
cybermeme <cybermeme@arch-libertas.net>
2021-03-14 21:38:50 +0000

    correction of the main tool from test to prod

--- confSeeker.py
+++ confSeeker.py
@@ -14,27 +14,28 @@ name with '_' even if it is not bests practices it is easier to me to read the c
 import libtmux
 import os
 import sys
-import pickle
-import random
 
 # LIBRARIES END #
 
 
 
+# PATH DEFINITION BEGIN #
 
-# If Ansible path in path, keep this path, else get the own directory
 if '/opt/addSofts/confSeeker/tools/' in os.environ['PATH']:
     base_tool_dir = ('/opt/addSofts/confSeeker/tools/')
 else:
     base_tool_dir = (os.getcwd() + '/tools/')
 
+# PATH DEFINITION END #
+
 
 
 # VARIABLES BEGIN #
 
-temporary_confseeker_file = '/tmp/confSeeker.tmp'
 base_folder = '/etc'
 default_file = '/etc/motd'
+#base_tool_dir = ('/opt/addSofts/confSeeker/tools/')
+#base_tool_dir = (os.getcwd() + '/tools/')
 defaut_list_file_tool = (base_tool_dir + 'cat_file.py')
 #defaut_list_file_tool = 'less'
 
@@ -43,44 +44,6 @@ defaut_list_file_tool = (base_tool_dir + 'cat_file.py')
 
 
 # DEFINITIONS BEGIN #
-
-def random_number():
-    return random.getrandbits(16)
-
-
-def ersase_config():
-    """efface la configuration dans un fichier"""
-    try:
-        os.system('rm ' + temporary_confseeker_file)
-        return 0
-    except:
-        exit("erasing Error")
-
-
-def save_config(dataSave):
-    """sauvegarde la configuration dans un fichier"""
-    try:
-        with open(temporary_confseeker_file, 'ab') as fichier:
-            mon_pickler = pickle.Pickler(fichier)
-            mon_pickler.dump(dataSave)
-            return 0
-    except:
-        exit("Saving Error")
-
-
-def load_config():
-    """charge la configuration depuis un fichier"""
-    with open(temporary_confseeker_file, 'rb') as fichier:
-        mon_depickler = pickle.Unpickler(fichier)
-        dataLoad = []
-        while True:
-            try:
-                dataLoad += [mon_depickler.load()]
-            except:
-                break
-        print('Data Read'.center(45, '*'))
-        return dataLoad
-
 
 def file_base_definition():
     '''' if no argument /etc ... we print the motd '''
@@ -104,7 +67,6 @@ def creation_two_panes_first_zone(folder=base_folder):
     rightPane = window.split_window(vertical=False)
     window.select_pane('%1') #choose which pane will have the hand
     underPane.send_keys('cd ' + folder + '; clear')
-    save_config(session)
 
 
 def load_from_file(file):
@@ -123,10 +85,8 @@ def triage(file):
     ''' sorts the type of configuration file and sends
         it to the right parser '''
     first_line = load_from_file(file)
-    
     if first_line.startswith('<?xml'):
         leftPane.send_keys(base_tool_dir + 'xml_parser.py '+ file)
-        
     else:
         leftPane.send_keys(base_tool_dir + 'unix_parser.py '+ file)
 
@@ -135,12 +95,10 @@ def creation_two_panes(file1 = '', file2 = ''):
     ''' launches the creation_two_panes of the tmux display and 
         launches the display in the panes '''
     creation_two_panes_first_zone()
-    
     if file1 and file2:
         rightPane.send_keys(defaut_list_file_tool + ' ' + (file1))
         leftPane.send_keys(defaut_list_file_tool + ' ' + (file2))
         underPane.send_keys('diff ' + file1 + ' ' + file2)
-        
     else:
         #print in the right panes
         rightPane.send_keys(defaut_list_file_tool + ' ' +\
@@ -152,31 +110,17 @@ def creation_two_panes(file1 = '', file2 = ''):
 
 
 def close_session():
-    ''' Close the window '''
-    server = libtmux.Server()
-    session = load_config()
-    print(window)
-    session.kill_window()
-
-
-def exit_session():
     ''' Close the software '''
     server = libtmux.Server()
-    ersase_config()
-    server.kill_session('com_comander')
+    session = server.kill_session('com_comander')
 
 
 def com_comander():
     ''' main function parsing the arguments '''
     if len(sys.argv) == 4 and sys.argv[1] == "diff":
         creation_two_panes(sys.argv[2], sys.argv[3])
-        
     elif len(sys.argv) == 2 and (sys.argv[1] == "help" or sys.argv[1] == "-h"):
         help_com_comander()
-  
-    elif len(sys.argv) == 2 and sys.argv[1] == "close":
-        close_session()
-        
     else:
         #if no session create one
         try:
@@ -190,10 +134,8 @@ def com_comander_parser():
     ''' parse the arguments
         I chose to do it manually rather than using argparse in order to be 
         able to launch the environment without argument.'''
-        
-    if len(sys.argv) == 2 and sys.argv[1] == "quit":
-        exit_session() 
-        
+    if len(sys.argv) == 2 and (sys.argv[1] == "close" or sys.argv[1] == "quit"):
+        close_session()
     elif len(sys.argv) == 2 and sys.argv[1] == "help":
         help_com_comander() 
 
@@ -203,8 +145,7 @@ def help_com_comander():
     print('\n\tUsage : ' + sys.argv[0] + ' /path/to/file/to/parse\n\n' \
           + sys.argv[0] + ' help = this message in the commander\n' \
           + sys.argv[0] + ' diff /path/to/file1 /path/to/file2 = diff 2 files\n' \
-          + sys.argv[0] + ' close = close the window\n' \
-          + sys.argv[0] + ' quit = close the tool\n')
+          + sys.argv[0] + ' [close|quit] = close the tool\n')
 
 # DEFINITIONS END #
 
@@ -217,7 +158,6 @@ uid = os.getuid()
 
 if uid == 0:
     os.setuid(uid) 
-
-#Let's go
+    
 com_comander()
 
